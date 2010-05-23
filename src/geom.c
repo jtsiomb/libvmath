a0dc13cfe20e36328889ac5faf47b11a592e3443
jtsiomb <none@none>
2010-05-23 23:27:16 +0000

    adding geometry code
    --HG--
    branch : libvmath
    extra : convert_revision : svn%3Aa63aed3b-50f6-46c9-a87c-b89ceb2d5030/libvmath%4054

--- src/geom.c
+++ src/geom.c
@@ -58,7 +58,7 @@ vec3_t plane_point(plane_t plane)
 	return v3_scale(plane.norm, plane.d);
 }
 
-scalar_t plane_ray_intersect(ray_t ray, plane_t plane)
+int plane_ray_intersect(ray_t ray, plane_t plane, scalar_t *pos)
 {
 	vec3_t pt, orig_to_pt;
 	scalar_t ndotdir;
@@ -67,9 +67,65 @@ scalar_t plane_ray_intersect(ray_t ray, plane_t plane)
 	ndotdir = v3_dot(plane.norm, ray.dir);
 
 	if(fabs(ndotdir) < 1e-7) {
-		return -1.0;
+		return 0;
 	}
 
-	orig_to_pt = v3_sub(pt, ray.origin);
-	return v3_dot(plane.norm, orig_to_pt) / ndotdir;
+	if(pos) {
+		orig_to_pt = v3_sub(pt, ray.origin);
+		*pos = v3_dot(plane.norm, orig_to_pt) / ndotdir;
+	}
+	return 1;
+}
+
+sphere_t sphere_cons(scalar_t x, scalar_t y, scalar_t z, scalar_t rad)
+{
+	sphere_t sph;
+	sph.pos.x = x;
+	sph.pos.y = y;
+	sph.pos.z = z;
+	sph.rad = rad;
+	return sph;
+}
+
+int sphere_ray_intersect(ray_t ray, sphere_t sph, scalar_t *pos)
+{
+	scalar_t a, b, c, d, sqrt_d, t1, t2, t;
+
+	a = v3_dot(ray.dir, ray.dir);
+	b = 2.0 * ray.dir.x * (ray.origin.x - sph.pos.x) +
+		2.0 * ray.dir.y * (ray.origin.y - sph.pos.y) +
+		2.0 * ray.dir.z * (ray.origin.z - sph.pos.z);
+	c = v3_dot(sph.pos, sph.pos) + v3_dot(ray.origin, ray.origin) +
+		2.0 * v3_dot(v3_neg(sph.pos), ray.origin) - sph.rad * sph.rad;
+
+	d = b * b - 4.0 * a * c;
+	if(d < 0.0) {
+		return 0;
+	}
+
+	sqrt_d = sqrt(d);
+	t1 = (-b + sqrt_d) / (2.0 * a);
+	t2 = (-b - sqrt_d) / (2.0 * a);
+
+	if(t1 < 1e-7 || t1 > 1.0) {
+		t1 = t2;
+	}
+	if(t2 < 1e-7 || t2 > 1.0) {
+		t2 = t1;
+	}
+	t = t1 < t2 ? t1 : t2;
+
+	if(t < 1e-7 || t > 1.0) {
+		return 0;
+	}
+
+	if(pos) {
+		*pos = t;
+	}
+	return 1;
+}
+
+int sphere_sphere_intersect(sphere_t sph1, sphere_t sph2, scalar_t *pos, scalar_t *rad)
+{
+	return -1;
 }
--- src/geom.h
+++ src/geom.h
@@ -9,10 +9,20 @@ typedef struct {
 	scalar_t d;
 } plane_t;
 
+typedef struct {
+	vec3_t pos;
+	scalar_t rad;
+} sphere_t;
+
+typedef struct {
+	vec3_t min, max;
+} aabox_t;
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/* planes are good... you need planes, yes you do */
 plane_t plane_cons(scalar_t nx, scalar_t ny, scalar_t nz, scalar_t d);
 plane_t plane_poly(vec3_t v0, vec3_t v1, vec3_t v2);
 plane_t plane_ptnorm(vec3_t pt, vec3_t normal);
@@ -23,7 +33,13 @@ scalar_t plane_signed_dist(plane_t plane, vec3_t pt);
 scalar_t plane_dist(plane_t plane, vec3_t pt);
 vec3_t plane_point(plane_t plane);
 
-scalar_t plane_ray_intersect(ray_t ray, plane_t plane);
+int plane_ray_intersect(ray_t ray, plane_t plane, scalar_t *pos);
+
+/* spheres always come in handy */
+sphere_t sphere_cons(scalar_t x, scalar_t y, scalar_t z, scalar_t rad);
+
+int sphere_ray_intersect(ray_t ray, sphere_t sph, scalar_t *pos);
+int sphere_sphere_intersect(sphere_t sph1, sphere_t sph2, scalar_t *pos, scalar_t *rad);
 
 #ifdef __cplusplus
 }
