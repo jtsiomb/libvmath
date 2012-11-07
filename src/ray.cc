aaebccc784cc2f8f88e35c4922c0e33fcd5d8a17
jtsiomb <none@none>
2012-11-07 22:36:10 +0000

    reverted a bad fix (rev 28) and fixed the formatting slightly in ray.cc
    --HG--
    branch : libvmath
    extra : convert_revision : svn%3Aa63aed3b-50f6-46c9-a87c-b89ceb2d5030/libvmath%40100

--- src/ray.cc
+++ src/ray.cc
@@ -3,14 +3,16 @@
 
 scalar_t Ray::env_ior = 1.0;
 
-Ray::Ray() {
+Ray::Ray()
+{
 	ior = env_ior;
 	energy = 1.0;
 	time = 0;
 	iter = 0;
 }
 
-Ray::Ray(const Vector3 &origin, const Vector3 &dir) {
+Ray::Ray(const Vector3 &origin, const Vector3 &dir)
+{
 	this->origin = origin;
 	this->dir = dir;
 	ior = env_ior;
@@ -19,31 +21,31 @@ Ray::Ray(const Vector3 &origin, const Vector3 &dir) {
 	iter = 0;
 }
 
-void Ray::transform(const Matrix4x4 &xform) {
-	Matrix4x4 upper;
-	Vector3 dir;
-
-	upper = xform;
+void Ray::transform(const Matrix4x4 &xform)
+{
+	Matrix4x4 upper = xform;
 	upper[0][3] = upper[1][3] = upper[2][3] = upper[3][0] = upper[3][1] = upper[3][2] = 0.0;
 	upper[3][3] = 1.0;
 
-	dir = (this->dir - origin).transformed(upper);
+	dir.transform(upper);
 	origin.transform(xform);
-	this->dir = dir + origin;
 }
 
-Ray Ray::transformed(const Matrix4x4 &xform) const {
+Ray Ray::transformed(const Matrix4x4 &xform) const
+{
 	Ray foo = *this;
 	foo.transform(xform);
 	return foo;
 }
 
-void Ray::enter(scalar_t new_ior) {
+void Ray::enter(scalar_t new_ior)
+{
 	ior = new_ior;
 	ior_stack.push(ior);
 }
 
-void Ray::leave() {
+void Ray::leave()
+{
 	if(ior_stack.empty()) {
 		//std::cerr << "empty ior stack?\n";
 		return;
